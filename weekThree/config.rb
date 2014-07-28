f06f146c099e622197b3db9ea6e16ac93b7cb7e8
GoddessC <goddesselixir@gmail.com>
2014-07-28 06:32:04 -0400

    Updating Porky

--- weekThree/config.rb
+++ weekThree/config.rb
@@ -7,7 +7,7 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
